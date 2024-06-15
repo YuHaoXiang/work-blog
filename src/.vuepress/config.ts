90d98c76c5e7b0e401c2d10e0ba1fcfb10b7e255
YuHaoXiang <398972900@qq.com>
2024-06-15 15:14:30 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/yhx.github.io/",
+  base: "/work-blog/",
 
   lang: "zh-CN",
   title: "文档演示",
